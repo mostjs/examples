61a6d7b6f70a4d4a4f44745a08af02824b558d7e
John Rees <john@bitsushi.com>
2017-07-05 11:57:47 +0100

    added extra comment about multicast

--- type-to-search/index.js
+++ type-to-search/index.js
@@ -13,6 +13,7 @@ const template = document.getElementById('template').innerHTML
 const getResults = text => rest(url + text).entity()
 
 // Get input value when it changes
+// Multicast the stream as it has more than one observer
 const searchText = input(search)
   .map(e => e.target.value.trim())
   .skipRepeats()
