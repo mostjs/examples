b1f50f4054002d23793173eb2d1442ae9aae8802
John Rees <johnrees@users.noreply.github.com>
2017-07-10 15:21:59 +0100

    feat(type-to-search): Handle an empty searchbox in Wikipedia example (#6)
    * consistent 2 spaces indentation
    
    * use https wikipedia URLs
    
    * Empty the results list if there is no search term present
    
    * added multicast() to searchText stream
    
    * only make API request if there is more than one character in query
    
    * added extra comment about multicast
    
    * semantics: changed stream observer comment to consumer
    
    * merge results and empty results streams into a single observer
    
    * remove unnecessary multicast()
    
    * remove unnecessary multicast comments
    
    * multicast the stream

--- type-to-search/index.js
+++ type-to-search/index.js
@@ -1,36 +1,47 @@
 import { input } from '@most/dom-event'
-import { map, filter, debounce, skipRepeats, switchLatest, fromPromise } from 'most'
+import { map, filter, debounce, skipRepeats, switchLatest, fromPromise, merge } from 'most'
 import rest from 'rest/client/jsonp'
 
-  const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
+const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
 
-  const search = document.getElementById('search')
-  const resultList = document.getElementById('results')
-  const template = document.getElementById('template').innerHTML
+const search = document.getElementById('search')
+const resultList = document.getElementById('results')
+const template = document.getElementById('template').innerHTML
 
-  // Fetch results with rest.js
-  // Returns a promise for the wikipedia json response
-  const getResults = text => rest(url + text).entity()
+// Fetch results with rest.js
+// Returns a promise for the wikipedia json response
+const getResults = text => rest(url + text).entity()
 
-  // Debounce keystrokes and get input value when it changes
-  // Only search if the user stopped typing for 500ms, and if the
-  // text is longer than 1 character and is different than the last
-  // time we saw the text.
-  const searchText = input(search)
-    .debounce(500)
-    .map(e => e.target.value.trim())
-    .filter(text => text.length > 1)
-    .skipRepeats()
+// Get input value when it changes
+// Multicast the stream as it's later being merged by an observer
+const searchText = input(search)
+  .map(e => e.target.value.trim())
+  .skipRepeats()
+  .multicast()
 
-  // Get results from wikipedia API and render
-  // Ignore empty results, extract the actual list of results
-  // from the wikipedia payload, then render the results
-  searchText.map(getResults)
-    .map(fromPromise)
-    .switch()
-    .filter(response => response.length > 1)
-    .map(response => response[1])
-    .observe(results => {
-      resultList.innerHTML = results.reduce((html, item) =>
-        html + template.replace(/\{name\}/g, item), '')
-    })
+// Get results from wikipedia API and render
+// Only search if the user stopped typing for 500ms
+// and is different than the last time we saw the text
+// Ignore empty results, extract and return the actual
+// list of results from the wikipedia payload
+const results = searchText
+  .filter(text => text.length > 1)
+  .debounce(500)
+  .map(getResults)
+  .map(fromPromise)
+  .switch()
+  .filter(results => results.length > 1)
+  .map(results => results[1])
+
+// Empty results list if there is no search term
+const emptyResults = searchText
+  .filter(text => text.length < 1)
+  .constant([])
+
+// Render the results
+merge(results, emptyResults)
+  .observe(resultContent => {
+    resultList.innerHTML = resultContent.reduce(
+      (html, item) => html + template.replace(/\{name\}/g, item), ''
+    )
+  })
