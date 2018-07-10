86d5c9840c16f0de2c674d2a1b9138bec20b0b7b
Brian Cavalier <brian@briancavalier.com>
2018-07-10 07:46:20 -0400

    Port type-to-search

--- type-to-search/index.js
+++ type-to-search/index.js
@@ -1,5 +1,6 @@
+import { map, multicast, swithcLatest, constant, runEffects, filter, debounce, skipRepeats, switchLatest, fromPromise, merge, tap } from '@most/core'
+import { newDefaultScheduler } from '@most/scheduler'
 import { input } from '@most/dom-event'
-import { map, filter, debounce, skipRepeats, switchLatest, fromPromise, merge } from 'most'
 import rest from 'rest/client/jsonp'
 
 const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
@@ -14,34 +15,27 @@ const getResults = text => rest(url + text).entity()
 
 // Get input value when it changes
 // Multicast the stream as it's later being merged by an observer
-const searchText = input(search)
-  .map(e => e.target.value.trim())
-  .skipRepeats()
-  .multicast()
+const searchText = multicast(
+  skipRepeats(
+    map(e => e.target.value.trim(), input(search))))
 
 // Get results from wikipedia API and render
 // Only search if the user stopped typing for 500ms
 // and is different than the last time we saw the text
 // Ignore empty results, extract and return the actual
 // list of results from the wikipedia payload
-const results = searchText
-  .filter(text => text.length > 1)
-  .debounce(500)
-  .map(getResults)
-  .map(fromPromise)
-  .switch()
-  .filter(results => results.length > 1)
-  .map(results => results[1])
+const results = map(results => results[1], filter(results => results.length > 1, switchLatest(map(fromPromise, map(getResults, debounce(500, filter(text => text.length > 1, searchText)))))))
 
 // Empty results list if there is no search term
-const emptyResults = searchText
-  .filter(text => text.length < 1)
-  .constant([])
+const emptyResults = constant([], filter(text => text.length < 1, searchText))
+
+const updates = merge(results, emptyResults)
+
+const render = resultContent => {
+  resultList.innerHTML = resultContent.reduce(
+    (html, item) => html + template.replace(/\{name\}/g, item), ''
+  )
+}
 
 // Render the results
-merge(results, emptyResults)
-  .observe(resultContent => {
-    resultList.innerHTML = resultContent.reduce(
-      (html, item) => html + template.replace(/\{name\}/g, item), ''
-    )
-  })
+runEffects(tap(render, updates), newDefaultScheduler())
